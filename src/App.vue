2a1e8ec2efcc5f5bd55a2e7f3ec33d9e76104549
Max Bogue <max@maxbogue.com>
2019-01-20 16:49:38 -0600

    Waveform component.

--- src/App.vue
+++ src/App.vue
@@ -22,37 +22,24 @@
         :samples-per-bar="samplesPerBar"
         @update="updatePeaks"
       />
-      <button @click="downloadSvg">Download</button>
     </div>
-    <svg
-      ref="svg"
-      width="100%"
-      preserveAspectRatio="none"
-      xmlns="http://www.w3.org/2000/svg"
-      shape-rendering="crispEdges"
-    ></svg>
+    <waveform
+      :peaks="peaks"
+      :num-rows="numRows"
+      :row-height="rowHeight"
+      :bars-per-row="barsPerRow"
+    />
   </div>
 </template>
 
 <script>
-import throttle1 from 'lodash/fp/throttle';
-import { saveAs } from 'file-saver';
-
 import LoadAudio from './LoadAudio.vue';
-
-const SVG_NS = 'http://www.w3.org/2000/svg';
-
-const throttle = throttle1.convert({ fixed: false });
-
-function removeAllChildNodes(el) {
-  while (el.firstChild) {
-    el.removeChild(el.firstChild);
-  }
-}
+import Waveform from './Waveform.vue';
 
 export default {
   components: {
     LoadAudio,
+    Waveform,
   },
   data: () => ({
     peaks: null,
@@ -62,73 +49,13 @@ export default {
     samplesPerBar: 100,
   }),
   computed: {
-    totalHeight() {
-      return this.rowHeight * this.numRows;
-    },
-    halfRowHeight() {
-      return this.rowHeight / 2;
-    },
     numBars() {
       return this.barsPerRow * this.numRows;
     },
   },
-  watch: {
-    peaks: 'render',
-    numRows: 'render',
-    rowHeight: 'render',
-  },
-  mounted() {
-    this.resizeSvg();
-  },
   methods: {
-    resizeSvg() {
-      this.$refs.svg.setAttribute('height', this.totalHeight);
-      this.$refs.svg.setAttribute(
-        'viewBox',
-        `0 0 ${this.barsPerRow} ${this.totalHeight}`
-      );
-    },
     updatePeaks(peaks) {
       this.peaks = peaks;
-    },
-    render: throttle(
-      200,
-      function() {
-        /* eslint-disable no-invalid-this */
-        removeAllChildNodes(this.$refs.svg);
-        if (!this.peaks) {
-          return;
-        }
-        this.resizeSvg();
-        for (let i = 0; i < this.numRows; i++) {
-          const y = i * this.rowHeight + this.halfRowHeight;
-          for (let j = 0; j < this.barsPerRow; j++) {
-            const k = 2 * (i * this.barsPerRow + j);
-            this.renderBar(j, y, this.peaks[k], this.peaks[k + 1]);
-          }
-        }
-        /* eslint-enable no-invalid-this */
-      },
-      { leading: false }
-    ),
-    renderBar(x, y, min, max) {
-      const line = document.createElementNS(SVG_NS, 'line');
-      const maxY = y - max * this.halfRowHeight;
-      const minY = y - min * this.halfRowHeight;
-      line.setAttributeNS(null, 'x1', x);
-      line.setAttributeNS(null, 'x2', x);
-      line.setAttributeNS(null, 'y1', maxY);
-      line.setAttributeNS(null, 'y2', minY);
-      line.setAttributeNS(null, 'stroke', 'black');
-      this.$refs.svg.appendChild(line);
-    },
-    downloadSvg() {
-      saveAs(
-        new Blob([this.$refs.svg.outerHTML], {
-          type: 'application/svg+xml',
-        }),
-        'waveform-' + Date.now() + '.svg'
-      );
     },
   },
 };
---
+++ src/Waveform.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <svg
+      ref="svg"
+      width="100%"
+      preserveAspectRatio="none"
+      xmlns="http://www.w3.org/2000/svg"
+      shape-rendering="crispEdges"
+    ></svg>
+    <button @click="downloadSvg">Download</button>
+  </div>
+</template>
+
+<script>
+import throttle1 from 'lodash/fp/throttle';
+import { saveAs } from 'file-saver';
+
+const SVG_NS = 'http://www.w3.org/2000/svg';
+
+const throttle = throttle1.convert({ fixed: false });
+
+function removeAllChildNodes(el) {
+  while (el.firstChild) {
+    el.removeChild(el.firstChild);
+  }
+}
+
+export default {
+  props: {
+    peaks: { type: Array, default: null },
+    numRows: { type: Number, required: true },
+    rowHeight: { type: Number, required: true },
+    barsPerRow: { type: Number, required: true },
+  },
+  computed: {
+    totalHeight() {
+      return this.rowHeight * this.numRows;
+    },
+    halfRowHeight() {
+      return this.rowHeight / 2;
+    },
+  },
+  watch: {
+    peaks: 'render',
+    numRows: 'render',
+    rowHeight: 'render',
+  },
+  mounted() {
+    this.resizeSvg();
+  },
+  methods: {
+    resizeSvg() {
+      if (!this.totalHeight) {
+        return;
+      }
+      this.$refs.svg.setAttribute('height', this.totalHeight);
+      this.$refs.svg.setAttribute(
+        'viewBox',
+        `0 0 ${this.barsPerRow} ${this.totalHeight}`
+      );
+    },
+    render: throttle(
+      200,
+      function() {
+        /* eslint-disable no-invalid-this */
+        removeAllChildNodes(this.$refs.svg);
+        if (!this.peaks) {
+          return;
+        }
+        this.resizeSvg();
+        for (let i = 0; i < this.numRows; i++) {
+          const y = i * this.rowHeight + this.halfRowHeight;
+          for (let j = 0; j < this.barsPerRow; j++) {
+            const k = 2 * (i * this.barsPerRow + j);
+            this.renderBar(j, y, this.peaks[k], this.peaks[k + 1]);
+          }
+        }
+        /* eslint-enable no-invalid-this */
+      },
+      { leading: false }
+    ),
+    renderBar(x, y, min, max) {
+      const line = document.createElementNS(SVG_NS, 'line');
+      const maxY = y - max * this.halfRowHeight;
+      const minY = y - min * this.halfRowHeight;
+      line.setAttributeNS(null, 'x1', x);
+      line.setAttributeNS(null, 'x2', x);
+      line.setAttributeNS(null, 'y1', maxY);
+      line.setAttributeNS(null, 'y2', minY);
+      line.setAttributeNS(null, 'stroke', 'black');
+      this.$refs.svg.appendChild(line);
+    },
+    downloadSvg() {
+      saveAs(
+        new Blob([this.$refs.svg.outerHTML], {
+          type: 'application/svg+xml',
+        }),
+        'waveform-' + Date.now() + '.svg'
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.container {
+  margin: 20px auto;
+  width: 600px;
+}
+.controls {
+  label {
+    display: block;
+  }
+  input[type='number'] {
+    width: 50px;
+  }
+}
+</style>
